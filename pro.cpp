49dcdff23c47dbb97ab13f8ba1283a87dfa77d6a
Ashwani0044 <ashwanib707@gmail.com>
2026-03-28 00:03:39 +0530

    basic task queue

---
+++ pro.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+#include <functional>//functional is used for std::greater
+
+using namespace std;
+
+queue<function<void()>> tasks;
+
+void executeTasks() {
+    while(!tasks.empty()) {
+        auto task = tasks.front();
+        tasks.pop();
+
+        task();
+    }
+}
+
+
+int main() {
+
+    //add tasks
+    tasks.push([]() {
+        cout<<"Task 1 executed..\n";
+    });
+
+    tasks.push([]() {
+        cout<<"Task 2 executed..\n";
+    });
+
+    //execute tasks
+    executeTasks();
+
+    return 0;
+}
